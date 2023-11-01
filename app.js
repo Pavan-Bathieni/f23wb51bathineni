950f8a0923f3fbb08ba8a34f8271d562836261fd
Pavan-Bathieni <S566651@nwmissouri.edu>
2023-11-01 15:31:39 -0500

    Added choose View

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var watchRouter = require('./routes/Watch');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose')
 var app = express();
 
 // view engine setup
@@ -24,7 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Watch',watchRouter);
 app.use('/board',boardRouter);
-
+app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
